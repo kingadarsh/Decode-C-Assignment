51a6b7c49b17aea21a6a8fdc0245a1376e937594
kingadarsh <adarsh261201@gmail.com>
2025-01-25 20:53:47 +0530

    Initial Commit

---
+++ Cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cpp
+//
+//  Created by Adarsh Kathriya on 25/01/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
